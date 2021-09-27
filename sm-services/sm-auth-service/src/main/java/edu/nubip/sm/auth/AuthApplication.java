5e74ef0fa40d58f747bac707282e4cfc3bc2786a
Bohdan Krupko <bkrupko@agroprosperis.com>
2021-09-27 18:52:12 +0300

    Dictionary service initial commit

--- sm-services/sm-auth-service/src/main/java/edu/nubip/sm/auth/AuthApplication.java
+++ sm-services/sm-auth-service/src/main/java/edu/nubip/sm/auth/AuthApplication.java
@@ -8,9 +8,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableResourceServer
-public class Application {
+public class AuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
---
+++ sm-services/sm-dictionary-service/src/main/java/edu/nubip/sm/dictionary/DictionaryApplication.java
@@ -0,0 +1,11 @@
+package edu.nubip.sm.dictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DictionaryApplication.class, args);
+    }
+}
