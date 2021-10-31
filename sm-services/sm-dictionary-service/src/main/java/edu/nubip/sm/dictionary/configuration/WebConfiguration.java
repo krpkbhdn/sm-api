7731cd3c0402897db81d088d43b91d661f500a39
Bohdan Krupko <bkrupko@agroprosperis.com>
2021-11-01 00:52:40 +0200

    feat(sm-services): kafka

--- sm-services/sm-dictionary-service/src/main/java/edu/nubip/sm/dictionary/configuration/WebConfiguration.java
+++ sm-services/sm-dictionary-service/src/main/java/edu/nubip/sm/dictionary/configuration/WebConfiguration.java
@@ -1,12 +1,10 @@
 package edu.nubip.sm.dictionary.configuration;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@Slf4j
 public class WebConfiguration {
 
     @Bean
--- sm-services/sm-processing-service/src/main/java/edu/nubip/sm/processing/consumer/KafkaConsumer.java
+++ sm-services/sm-processing-service/src/main/java/edu/nubip/sm/processing/consumer/KafkaConsumer.java
@@ -4,13 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 @Slf4j
 public class KafkaConsumer {
 
     @KafkaListener(topics = "sm-dictionary")
     public void processDictionaryMessages(String message) {
-        System.out.println(message);
+        log.info(message);
     }
 
 }
